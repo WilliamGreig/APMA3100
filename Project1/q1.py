7b37b0e2f15475cf33ee24026888d62ec784aa2a
WilliamGreig <wpg6zmk@virginia.edu>
2022-01-26 22:36:00 -0500

    initial commit...

---
+++ Project1/q1.py
@@ -0,0 +1,41 @@
+import itertools
+
+orders = 0
+books = ['calculus', 'physics', 'chemistry', 'history', 'literature', 'french', 'astronomy']
+books.sort()
+s = []
+for a in books:
+    for b in books:
+        if a != b:
+            for c in books:
+                if c != a and c != b:
+                    for d in books:
+                        if d != a and d != b and d != c:
+                            orders += 1
+                            s.append([a, b, c, d])
+
+print(orders)
+for x in s:
+    print(x)
+    pass
+
+print("----------------------------")
+
+book_orders = []
+for a in books:
+    for b in books:
+        if a != b:
+            for c in books:
+                if c != a and c != b:
+                    for d in books:
+                        if d != a and d != b and d != c:
+                            orders += 1
+                            t_order = [a, b, c, d]
+                            t_order.sort()
+                            if t_order not in book_orders:
+                                book_orders.append(t_order)
+
+for x in book_orders:
+    print(x)
+
+print(len(book_orders))
