a406ece8ccb9156b3c0d17d5ae849a9275a16a1c
William Greig <wpg6zmk@virginia.edu>
2022-04-29 22:41:41 -0400

    APMA3100 Update

---
+++ Project3/experiment.py
@@ -0,0 +1,148 @@
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+
+from math import *
+def phi(x):
+    #'Cumulative distribution function for the standard normal distribution'
+    return (1.0 + erf(x / sqrt(2.0))) / 2.0
+
+x_0 = 1000
+a = 24693
+c = 3967
+K = math.pow(2, 18)
+
+x_list = [x_0]
+u_list = []
+
+def rng():
+    # get most recent value
+    i = len(x_list)
+    value = a * x_list[i - 1] + c
+    remainder = value % K
+    x_list.append(remainder)
+    u_list.append(x_list[i] / K)
+    return u_list[len(u_list) - 1]
+
+# takes probability and returns distance -- inverse of CDF of X
+def inverse(p):
+    tau = 57
+    a_0 = 1 / tau
+    q = (-2) * np.log(1 - p) / math.pow(a_0, 2)
+    return math.sqrt(q)
+
+
+# performs monte_carlo simulation on n different realizations of X
+def monte_carlo(s):
+    xSum = 0
+    for i in range(s):
+        # realization of X
+        X = inverse(rng())
+        xSum = xSum + X
+    samp_mean = xSum / s
+    return samp_mean
+
+sample_sizes = [3, 9, 27, 81]
+results = []
+for samp_size in sample_sizes:
+    sub_result = []
+    for i in range(550):
+        sub_result.append(monte_carlo(samp_size))
+    results.append(sub_result)
+
+# calculate stage
+list_mus = []
+list_variance = []
+# calculate mu
+for outcome in results:
+    # outcome is list of values
+    cur_sum = 0
+    for ele in outcome:
+       cur_sum = cur_sum + ele
+    list_mus.append(cur_sum/len(outcome))
+
+# calculate variance
+index = 0
+for outcome in results:
+    cur_sum = 0
+    for ele in outcome:
+        cur_sum = math.pow(ele, 2) - math.pow(list_mus[index], 2)
+    list_variance.append(cur_sum/len(outcome))
+    index = index + 1
+
+# Transform stage
+index = 0
+for outcome in results:
+    t_index = 0
+    for element in outcome:
+        results[index][t_index] = (results[index][t_index] - list_mus[index]) / math.sqrt(list_variance[index])
+        t_index = t_index + 1
+    index = index + 1
+
+for eq in results:
+    print(eq)
+
+z = [-1.4 , -1, -0.5, 0, 0.5, 1, 1.4]
+phi_z = [1 - 0.9192, 1 - 0.8413, 1 - 0.6195, 0.5, 0.6195, 0.8413, 0.9192 ]
+z_value_results = []
+index = 0
+for outcome in results:
+    print("n={}".format(sample_sizes[index]))
+    temp_z = []
+    for z_j in z:
+        count = 0
+        for element in outcome:
+            if element <= z_j:
+                count = count + 1
+        print("\[z = {}: {}\]".format(z_j, round(count/len(outcome), 3)))
+        temp_z.append(count/len(outcome))
+    z_value_results.append(temp_z)
+    index = index + 1
+
+
+count = 0
+for ls in z_value_results:
+    max = 0
+    index = 0
+    z_jIndex = 0
+    for z_vals in ls:
+        if abs(z_vals - phi_z[index]) > max:
+            max = abs(z_vals - phi_z[index])
+            z_jIndex = index
+        index = index + 1
+    print("z_jIndex value: {}".format(ls[z_jIndex]))
+    print("max value {}".format(max))
+    count = count + 1
+
+Phi_Xvals = []
+Phi_Yvals = []
+
+curr_x = -2.5
+increment = 0.01
+while curr_x < 2.5:
+    Phi_Xvals.append(curr_x)
+    Phi_Yvals.append(phi(curr_x))
+    curr_x = curr_x + increment
+
+figure, axes = plt.subplots()
+plt.plot(z, z_value_results[0], "-o", color="blue", label="blue: n=3")
+plt.axvspan(0.48545454545454547, 0.4046545454545455, color='blue', alpha=0.3, label="MAD3 interval")
+plt.plot(z, z_value_results[1],"-o", color="red", label="red: n=9")
+plt.axvspan(0.5727272727272728, 0.34647272727272727, color='red', alpha=0.3, label="MAD9 interval")
+plt.plot(z, z_value_results[2],"-o", color="green", label="green: n=27")
+plt.axvspan(0.4381818181818182, 0.3573818181818182, color='green', alpha=0.3, label="MAD27 interval")
+plt.plot(z, z_value_results[3],"-o", color="black", label="black: n=81")
+plt.axvspan(0.42727272727272725, 0.34647272727272727, color='black', alpha=0.3, label="MAD81 interval")
+plt.plot(Phi_Xvals, Phi_Yvals, color="black", label="Normal CDF Phi(z)")
+
+plt.title('Empericial CDF of Estimated Means, Normal CDF, and Probability Interval of MAD(n)')
+plt.xlabel('Standardized Z-Values')
+plt.ylabel('Probability')
+
+plt.legend()
+plt.xlim([-2.5, 2.5])
+plt.show()
+
+
+
--- Project3/monte-carlo.py
+++ Project3/monte-carlo.py
@@ -1,4 +1,7 @@
 import math
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 
 x_0 = 1000
 a = 24693
@@ -17,5 +20,116 @@ def rng():
     u_list.append(x_list[i] / K)
     return u_list[len(u_list) - 1]
 
-for i in range(20):
-    print("u_{}: {}".format(i, rng()))
+""" Numbers for Report:
+u_51: 0.19949
+u_52: 0.20005
+u_53: 0.04691
+"""
+
+# takes probability and returns distance -- inverse of CDF of X
+def inverse(p):
+    tau = 57
+    a_0 = 1 / tau
+    q = (-2) * np.log(1 - p) / math.pow(a_0, 2)
+    return math.sqrt(q)
+
+# performs monte_carlo simulation on n different realizations of X
+def monte_carlo(s):
+    xSum = 0
+    for i in range(s):
+        # realization of X
+        X = inverse(rng())
+        xSum = xSum + X
+    samp_mean = xSum / s
+    return samp_mean
+
+# performs n monte carlo simulations for a sample size of s
+def repeated_monte_carlo(n, s):
+    esti_sum = 0
+    for i in range(n):
+        monte_result = monte_carlo(s)
+        esti_sum = esti_sum + monte_result
+    estimated_mean = esti_sum / n
+
+    return estimated_mean
+
+realizations = 110
+list_of_samples = [10, 140, 50, 100, 250, 500, 1000]
+# n -- number of samples
+x_axis = []
+# m_n -- result of monte-carlo
+y_axis = []
+count = 0
+for x in list_of_samples:
+    # x is sample size, realizations is the number of repeated monte carlo simulations
+    for i in range(realizations):
+        y_axis.append(monte_carlo(x))
+        x_axis.append(x)
+        count = count + 1
+
+colors = ["red", "orange", "green", "blue", "purple", "pink", "black"]
+figure, axes = plt.subplots()
+scatter_dots10 = plt.scatter(x_axis[0 : 110], y_axis[0:110], color=colors[0])
+scatter_dots30 = plt.scatter(x_axis[110 : 220], y_axis[110:220], color=colors[1])
+scatter_dots50 = plt.scatter(x_axis[220 : 330], y_axis[220 : 330], color=colors[2])
+scatter_dots100 = plt.scatter(x_axis[330 : 440], y_axis[330 : 440], color=colors[3])
+scatter_dots250 = plt.scatter(x_axis[440 : 550], y_axis[440 : 550], color=colors[4])
+scatter_dots500 = plt.scatter(x_axis[550 : 660], y_axis[550 : 660], color=colors[5])
+scatter_dots1000 = plt.scatter(x_axis[660 : 770], y_axis[660 : 770], color=colors[6])
+mux = plt.axhline(y=71.4389, color='black', linestyle='-')
+mux2 = plt.axhline(y=71.4389 + 10, color='black', linestyle='-')
+mux3 = plt.axhline(y=71.4389 - 10, color='black', linestyle='-')
+axes.legend([scatter_dots10, scatter_dots30, scatter_dots50, scatter_dots100, scatter_dots250, scatter_dots500, scatter_dots1000, mux], ['red: n = 10',
+                                                                                                                                    'orange: n = 30',
+                                                                                                                                    'green: n = 50',
+                                                                                                                                    'blue: n = 100',
+                                                                                                                                    'purple: n = 250',
+                                                                                                                                    'pink: n = 500',
+                                                                                                                                    'black: n = 1000',
+                                                                                                                                         "u_x=71.4389"])
+
+
+
+axes.add_artist( scatter_dots10 )
+axes.add_artist( scatter_dots30 )
+axes.add_artist( scatter_dots50 )
+axes.add_artist( scatter_dots100 )
+axes.add_artist( scatter_dots250 )
+axes.add_artist( scatter_dots500 )
+axes.add_artist( scatter_dots1000 )
+axes.add_artist( mux2 )
+axes.add_artist( mux3 )
+
+
+# naming the x axis
+plt.xlabel('Number of Samples')
+# naming the y axis
+plt.ylabel('Sample Estimation of X (inches)')
+
+
+# giving a title to my graph
+plt.title('Monte Carlo Sample Estimations across Various Sample Sizes')
+
+# function to show the plot
+# plt.show()
+
+for x in list_of_samples:
+    # x is sample size, realizations is the number of repeated monte carlo simulations
+    val = repeated_monte_carlo(realizations, x)
+    round_val = round(val, 3)
+    # print("\[est. \hspace[2pt] mean (n={}): {}\]".format(x, round_val))
+
+# calculate 7.
+succ = 0
+total = 0
+
+for i in range(10000):
+    val = monte_carlo(140)
+    abs_val = val - 71.4389
+    print(abs_val)
+    if abs(abs_val) < 10:
+        succ = succ + 1
+    total = total + 1
+print(succ)
+print(total)
+print("estimation: {}".format(succ/total))
